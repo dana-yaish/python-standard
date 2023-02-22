879ed0b83a15157dfa37f804a446466772679808
Dana <dana@codecov.io>
2023-02-22 15:21:51 +0200

    tst

--- src/test_index.py
+++ src/test_index.py
@@ -7,6 +7,10 @@ def test_uncovered_if():
 def test_fully_covered():
     assert index.fully_covered() == True
 
+def test1():
+    assert index.uncovered5() == False
 
+def test2():
+    assert index.uncovered4() == True
 
 
